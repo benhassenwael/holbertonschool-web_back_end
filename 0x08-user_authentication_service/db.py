626ace9eed4fdcf3efa010d2be7f51971bbe8ae7
benhassenwael <greenw2012@gmail.com>
2021-11-19 12:50:15 +0100

    rename: add_user => _add_user

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -32,7 +32,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> User:
+    def _add_user(self, email: str, hashed_password: str) -> User:
         """Save a user to the database
         """
         user = User(email=email, hashed_password=hashed_password)
